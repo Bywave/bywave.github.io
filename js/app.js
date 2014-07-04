872f7bae9d7347f89dfc68906c5db4cc0474d7ba
-xXLXx- <leo.lavarias@gmail.com>
2014-07-03 17:42:23 -0800

    #Page Loader
    #Navbar design

--- js/app.js
+++ js/app.js
@@ -1,14 +1,25 @@
 /**
 * Config
 */
-var durationNavAnim = 250;
-var durationNavHide = 500;
+var viewportHeight;
 /**
 * Fading header
 */
-var timeoutFadingHeader;
-var isFadingHeader = false;
+var timeoutFadingHeader,
+	isHeaderFading = false,
+	fadingHeader,
+	durationNavAnim = 250,
+	durationNavHide = 500;
+/**
+* Page Loader
+*/
+var pageloadDelay = 1000,
+	documentScrollTop,
+	mainPageId = '#main-page';
 
+/**
+* On Ready
+*/
 $(function(){
 	// Foundation
 	$(document).foundation();
@@ -23,48 +34,59 @@ $(function(){
 	/**
 	* Fading header
 	*/
-	initFadingHeaderEvents();
+	viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+	FadingHeader.init();
+
+	/**
+	* PageLoader
+	*/
+	initPageLoader();
 });
 
 /**
 * Fading header
 */
-function initFadingHeaderEvents(){
-	$homeSection = $('#section-home');
+var FadingHeader = {
+	init: function(){
+		fadingHeader = $('.fading-header');
+		viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
-	setInterval(function(){
-		if(!isElementInViewport($homeSection)){
-			$header = $('.fading-header');
+		setInterval(function(){
+			if(!isElementInViewport(document.documentElement)){
 
-			if($header.css("opacity") == '1' && isFadingHeader){
-				clearTimeout(timeoutFadingHeader);
-				isFadingHeader = false;
+				if(fadingHeader.css("opacity") == '1' && isHeaderFading){
+					clearTimeout(timeoutFadingHeader);
+					isHeaderFading = false;
+				}
+				else if(fadingHeader.css("opacity") == '0'){
+					FadingHeader.show();
+				}
 			}
-			else if($header.css("opacity") == '0'){
-				$header.animate({
-					opacity: 1,
-					marginTop: '0px'
-				}, durationNavAnim);
+			else{
+				if(fadingHeader.css("opacity") == '1' && !isHeaderFading){
+					isHeaderFading = true;
+					clearTimeout(timeoutFadingHeader);
+					timeoutFadingHeader = setTimeout(function(){
+						FadingHeader.hide();
+						isHeaderFading = false;
+					}, durationNavHide);
+				}
 			}
-		}
-		else{
-			$header = $('.fading-header');
-			
-			if($header.css("opacity") == '1' && !isFadingHeader){
-				isFadingHeader = true;
-				console.log('fade hedaer');
-				clearTimeout(timeoutFadingHeader);
-				timeoutFadingHeader = setTimeout(function(){
-					$header.animate({
-						opacity: 0,
-						marginTop: '-45px',
-					}, durationNavAnim);
-					isFadingHeader = false;
-				}, durationNavHide);
-			}
-		}
-	}, 100);
-}
+		}, 100);
+	},
+	hide: function(){
+		fadingHeader.animate({
+			opacity: 0,
+			marginTop: '-45px',
+		}, durationNavAnim);
+	},
+	show: function(){
+		fadingHeader.animate({
+			opacity: 1,
+			marginTop: '0px'
+		}, durationNavAnim);
+	},
+};
 
 function isElementInViewport (el) {
     if (el instanceof jQuery) {
@@ -72,4 +94,46 @@ function isElementInViewport (el) {
     }
     var rect = el.getBoundingClientRect();
     return rect.bottom > 50;
+}
+
+/**
+* Page Loader
+*/
+var pageWrap = $('.wrap')[0],
+	pages = [].slice.call( pageWrap.querySelectorAll( '.page' ) ),
+	currentPage = 0,
+	triggerLoading = [].slice.call( pageWrap.querySelectorAll( '.pageload-link' ) ),
+	loader = new SVGLoader($('#loader')[0], { speedIn : 400, easingIn : mina.easeinout } );
+	
+/**
+* Case Studies
+*/
+function initPageLoader() {
+	$('.pageload-link').on('click', function(ev){
+		ev.preventDefault();
+		loader.show();
+		$thisObj = $(this);
+
+		// after some time hide loader
+		setTimeout( function() {
+			loader.hide();
+
+			var origin = $thisObj.data('pageOrigin');
+			if(origin == mainPageId) documentScrollTop = $(document).scrollTop();
+			$origin = $(origin)[0].classList.toggle('show');
+
+			var destination = $thisObj.data('pageDestination');
+			$destination = $(destination)[0].classList.toggle('show');
+			if(destination == mainPageId) $(document).scrollTop(documentScrollTop);
+		}, pageloadDelay);
+	});
+
+	var tmpSpeedIn = loader.options.speedIn;
+	loader.options.speedIn = 0;
+	loader.show();
+	setTimeout(function(){
+		loader.options.speedIn = tmpSpeedIn;
+		loader.hide();
+		$('#main-page')[0].classList.toggle('show');
+	}, pageloadDelay);
 }
